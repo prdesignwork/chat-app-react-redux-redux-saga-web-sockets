9414e464d3903ef8c20579aee4b8684421cd083e
Paul Rodriguez <paul@prdesign.work>
2018-05-18 13:13:59 -0600

    Redux added, building components

--- chat/src/index.js
+++ chat/src/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import { Provider } from "react-redux";
+import { createSTore } from "redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import chat from "./reducers";
+
+const store = createStore(chat);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />,
+  </Provider>
+  document.getElementById("root")
+  );
 registerServiceWorker();
---
+++ chat/src/reducers/users.js
@@ -0,0 +1,14 @@
+import * as types from "../constants/ActionTypes";
+
+const users = (state = [], action) => {
+  switch (action.type) {
+    case types.ADD_USER:
+      return state.concat([{ name: action.name, ide: action.id }]);
+    case types.USERS_LIST:
+      return action.user;
+    default:
+      return state;
+  }
+};
+
+export default users;
